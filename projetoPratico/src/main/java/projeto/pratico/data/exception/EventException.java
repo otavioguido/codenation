47c3b66fab86e539b48304725bf18c36de8c8890
Otavio Silva <12450636+otavioguido@users.noreply.github.com>
2020-08-18 11:28:05 -0300

    Fixed DAO impl and interface, event model and service impl and interface and implemented filter endpoint

--- projetoPratico/src/main/java/projeto/pratico/data/exception/EventException.java
+++ projetoPratico/src/main/java/projeto/pratico/data/exception/EventException.java
@@ -1,11 +1,5 @@
 package projeto.pratico.data.exception;
 
-/**
- * https://stackify.com/java-custom-exceptions/
- * https://stackify.com/specify-handle-exceptions-java/
- * https://stackify.com/best-practices-exceptions-java/
- * https://stackify.com/common-mistakes-handling-java-exception/
- */
 public class EventException extends Exception {
 
     public EventException(String message, Throwable cause){
--- projetoPratico/src/main/java/projeto/pratico/data/repository/EventRepository.java
+++ projetoPratico/src/main/java/projeto/pratico/data/repository/EventRepository.java
@@ -1,25 +1,12 @@
 package projeto.pratico.data.repository;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import projeto.pratico.data.model.Event;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, UUID> {
-
-    List<Event> findByLogLevel(String logLevel, Pageable pageable);
-
-    List<Event> findByEventDescription(String eventDescription, Pageable pageable);
-
-    List<Event> findByEventLog(String eventLog, Pageable pageable);
-
-    List<Event> findByOrigin(String origin, Pageable pageable);
-
-    List<Event> findByCreatedAt(String createdAt, Pageable pageable);
-
-    List<Event> findByQuantity(String quantity, Pageable pageable);
+public interface EventRepository extends JpaRepository<Event, UUID>, JpaSpecificationExecutor<Event> {
 }
